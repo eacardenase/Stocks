0efe5fc7bbe93e4154bad8e22982cc442e654e0e
Edwin Cardenas <ecardenas@lwolf.com>
2025-01-06 19:05:44 -0500

    refactor: replace accessor method calls for dot notation syntax

--- Stocks/main.m
+++ Stocks/main.m
@@ -14,17 +14,17 @@ int main(int argc, const char * argv[]) {
         LWStockHolding *stock2 = [[LWStockHolding alloc] init];
         LWStockHolding *stock3 = [[LWStockHolding alloc] init];
         
-        [stock1 setPurchaseSharePrice:2.3];
-        [stock1 setCurrentSharePrice:4.5];
-        [stock1 setNumberOfShares:40];
+        stock1.purchaseSharePrice = 2.3;
+        stock1.currentSharePrice = 4.5;
+        stock1.numberOfShares = 40;
         
-        [stock2 setPurchaseSharePrice:12.19];
-        [stock2 setCurrentSharePrice:10.56];
-        [stock2 setNumberOfShares:90];
+        stock2.purchaseSharePrice = 12.19;
+        stock2.currentSharePrice = 10.56;
+        stock2.numberOfShares = 90;
         
-        [stock3 setPurchaseSharePrice:45.10];
-        [stock3 setCurrentSharePrice:49.51];
-        [stock3 setNumberOfShares:210];
+        stock3.purchaseSharePrice = 45.10;
+        stock3.currentSharePrice = 49.51;
+        stock3.numberOfShares = 210;
         
         NSArray *stocks = @[stock1, stock2, stock3];
         
